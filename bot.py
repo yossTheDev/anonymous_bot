61bcd463273d8017864cec9acb9627fe5c3f2cfd
yossTheDev <98903135+yossTheDev@users.noreply.github.com>
2022-03-09 14:53:02 -0500

    update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 from telegram import Update
 from telegram.ext import Updater , CommandHandler , CallbackContext
 
-def hello(update: Update , context : CallbackContext) -> None:
-     update.message.reply_text ( f 'Hello {update. effective_user . first_name }'
-  updater = Updater ( os.envirom["TOKEN"])
-  updater.dispatcher.add_handler ( CommandHandler( 'hello' , hello ))
-  updater.start_polling ()
-  updater.idle()
+about_message = """It was created by :     
+[Yoss THE DEV](https://t.me/yossthedev)
+
+[GitHub](https://github.com/yossTheDev/anonymous_bot)"""
+
+# About Message
+def about(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(about_message)
+    
+updater = Updater ( os.envirom["TOKEN"])
+updater.dispatcher.add_handler (CommandHandler( 'hello' , hello ))
+updater.start_polling ()
+updater.idle()
   
