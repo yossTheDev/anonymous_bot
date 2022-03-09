a84ab72530e9e87861cea43544b451ef2b2c87b2
Yoannis <yossthedev@gmail.com>
2022-03-09 18:05:45 +0100

    Anonymous Bot

---
+++ README.md
@@ -0,0 +1,7 @@
+# Anonymous Bot
+
+This bot helps you to convert the messages in your group to Anonymous messages
+
+[Link to the bot in Telegram](https://t.me/anonymous_4everbot)
+
+[Telegram](https://t.me/yossthedev)
--- main.py
+++ main.py
@@ -4,20 +4,50 @@ import os
 
 PORT = int(os.environ.get('PORT', 80))
 
+# Bot Messages
+help_message = """Is very easy to use :
+        
+➡ Add me as an administrator to a group               
+➡ Give me permission to delete messages
+➡ And see the magic 🎈🎈"""
+about_message = """It was created by :     
+[Yoss THE DEV](https://t.me/yossthedev)
+
+[GitHub](https://github.com/yossTheDev/anonymous_bot)"""
+welcome_message = """
+ I can help you to return all the
+ messages of your group in anonymous messages 🤫
+ 
+ press /help to see how to use it
+ press /about to learn about me"""
+
 
 def main():
-    def hello(update: Update, context: CallbackContext) -> None:
-        update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
+    # Method to answer messages from users
     def echo(update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+
+    # Welcome Message
+    def start(update: Update, context: CallbackContext) -> None:
+        update.message.reply_text("Hello" + update.effective_user.first_name + welcome_message)
+
+    # Help Message
+    def help(update: Update, context: CallbackContext) -> None:
+        update.message.reply_text(help_message)
+
+    # About Message
+    def about(update: Update, context: CallbackContext) -> None:
+        update.message.reply_text(about_message)
 
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 
     updater = Updater('5211534226:AAFd0VBARRIpGOqmFs1Agg0x7h0HBAajkUw')
 
-    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    # Add command handlers
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('about', about))
     updater.dispatcher.add_handler(echo_handler)
 
     updater.start_polling()
