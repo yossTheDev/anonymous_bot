eaf57d64e0931418be72444aee595aecdd0f0e6f
Yoannis <yossthedev@gmail.com>
2022-03-09 16:59:40 +0100

    Anonymous Bot

--- main.py
+++ main.py
@@ -2,29 +2,33 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import os
 
-PORT = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('PORT', 80))
 
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+def main():
+    def hello(update: Update, context: CallbackContext) -> None:
+        update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+    def echo(update: Update, context: CallbackContext):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+
+    updater = Updater('5211534226:AAFd0VBARRIpGOqmFs1Agg0x7h0HBAajkUw')
+
+    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    updater.dispatcher.add_handler(echo_handler)
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path="5211534226:AAFd0VBARRIpGOqmFs1Agg0x7h0HBAajkUw")
+
+    updater.bot.setWebhook(
+        'https://anonymous4everbot.herokuapp.com/' + "5211534226:AAFd0VBARRIpGOqmFs1Agg0x7h0HBAajkUw")
+
+    updater.idle()
 
 
-def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-    context.bot.delete_message(chat_id=update.effective_chat.id,message_id=update.message.message_id)
-
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-
-updater = Updater('5211534226:AAFd0VBARRIpGOqmFs1Agg0x7h0HBAajkUw')
-
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
-updater.dispatcher.add_handler(echo_handler)
-
-updater.start_webhook(listen="0.0.0.0",
-                      port=int(PORT),
-                      url_path="5211534226:AAFd0VBARRIpGOqmFs1Agg0x7h0HBAajkUw")
-
-updater.bot.setWebhook('https://anonymous4everbot.herokuapp.com/' + "5211534226:AAFd0VBARRIpGOqmFs1Agg0x7h0HBAajkUw")
-
-updater.idle()
+if __name__ == '__main__':
+    main()
