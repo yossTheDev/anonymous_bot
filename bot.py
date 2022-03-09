07b9051d2acc81dca6963f5cd683ed6bd3ef9b03
yossTheDev <98903135+yossTheDev@users.noreply.github.com>
2022-03-09 14:49:57 -0500

    update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from telegram import Update
 from telegram.ext import Updater , CommandHandler , CallbackContext
 
-def hello ( update: Update , context : CallbackContext ) -> None:
-  update.message.reply_text ( f 'Hello {update. effective_user . first_name }'
+def hello(update: Update , context : CallbackContext) -> None:
+     update.message.reply_text ( f 'Hello {update. effective_user . first_name }'
   updater = Updater ( os.envirom["TOKEN"])
-  updater.dispatcher .add_handler ( CommandHandler ( 'hello' , hello ))
+  updater.dispatcher.add_handler ( CommandHandler( 'hello' , hello ))
   updater.start_polling ()
   updater.idle()
   
