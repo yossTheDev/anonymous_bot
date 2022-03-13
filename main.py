dff21a14bf46da07766405d1a522212627e6fd42
yossTheDev <98903135+yossTheDev@users.noreply.github.com>
2022-03-13 09:37:15 -0400

    update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ press /help to see how to use it
 press /about to learn about me"""
 
 def test(update: Update, context: CallbackContext) -> None:
-        update.message.reply_text(requests.get('https://uvs.ucm.cmw.sld.cu/ ').text)
+        update.message.reply_text(requests.get('https://uvs.ucm.cmw.sld.cu/ ').url)
 
 def main():
     # Method to answer messages from users
